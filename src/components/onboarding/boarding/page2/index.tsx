2192009319fc3a194d964b87d7a1562865e354e1
mohammadpiriyan <mohammadpiriyan@gmail.com>
2024-12-05 17:04:55 +0330

    create Page2 component

---
+++ src/components/onboarding/boarding/page2/index.tsx
@@ -0,0 +1,30 @@
+interface Page2Props {
+  setPage: (value: number) => void;
+}
+const Page2: React.FC<Page2Props> = ({ setPage }) => {
+  return (
+    <div className="w-full h-screen flex flex-col items-center ">
+      <div className="w-[430px] h-[60%]">
+        <img src="images/shoePage1.png" alt="" className="w-full h-full" />
+      </div>
+      <div className="relative w-[430px] h-full flex flex-col items-center gap-16 pt-8 px-6">
+        <p className="text-2xl font-semibold text-center">
+          Your satisfaction is our number one periority
+        </p>
+        <div className=" flex gap-2">
+          <div className="w-8 h-1 bg-black opacity-50"></div>
+          <div className="w-8 h-1 bg-black"></div>
+          <div className="w-8 h-1 bg-black opacity-50"></div>
+        </div>
+        <button
+          onClick={() => setPage(2)}
+          className="bg-black hover:bg-gray-800 rounded-full text-white w-full py-3 absolute bottom-8"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Page2;
